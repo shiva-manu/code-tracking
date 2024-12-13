800e52b9c661364eb913ad21da034e3575617ec4
shiva-manu <shivamani6964@gmail.com>
2024-12-14 00:17:25 +0530

    DevTrack: Files: Table.java, Test.java, TestSynchronization.java, ThreadReadDemo.java. | 2 added, 2 deleted

---
+++ problem8/ThreadReadDemo.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class ThreadReadDemo{
+   public static void main(String args[]){
+   }
+}
+
+class MultiThread implements Runnable{
+   private static BufferedReader br=null;
+   private List<String> list;
+   static{
+      try{
+         
+      }
+   }
+
+}
