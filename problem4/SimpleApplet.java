5055949b33da5e395e7cd45645e791014fba09c0
shiva-manu <shivamani6964@gmail.com>
2024-12-13 21:49:17 +0530

    DevTrack: Initial commit

--- problem4/SimpleApplet.java
+++ problem4/SimpleApplet.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 import java.applet.Applet;
-// <applet code="SimpleApplet" width=300 height=50></applet>
+// <applet code="SimpleApplet" width=1000 height=1000></applet>
 public class SimpleApplet extends Applet{
    public void paint(Graphics g){
       g.drawString("A Simple Applet",100,100);
